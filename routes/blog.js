3d07af7d668d0ebc1c6a8cfc48c9f2efa08e8c63
nyarosu <ronakc@fb.com>
2022-11-24 11:58:59 +0000

    finish token checking when creating post, and add CORS

--- routes/blog.js
+++ routes/blog.js
@@ -12,7 +12,7 @@ const generateToken = require("../utilities/generateToken")
 blogRouter.get("/", async (req, res) => {
     try {
         const query = await Blog.findAll()
-        res.send(query)
+        res.send({success: true, blogs: query})
     } catch {
         res.send({success: false})
     }
@@ -23,19 +23,28 @@ blogRouter.put("/", async (req, res) => {
     try {
         // Generate a URL, then create a new blog database entry 
         url = generateURL(req.body.blogTitle)
-        const response = await Blog.create({name: req.body.blogTitle, password: req.body.password, address: url})
+        const newBlog = await Blog.create({name: req.body.blogTitle, password: req.body.password, address: url})
+        
+        // Generate a token for this user, and associate the blog with the token
+        const generatedToken = generateToken()
+        console.log(generatedToken)
+        newToken = await Token.create({token: generatedToken})
+        
+        await newBlog.addToken(newToken)
+
         res.send({success: true, blogAddress: url})
-    } catch {
-        res.send({success: false, blogAddress: undefined, error: "An error occured."})
+    } catch (err) {
+        res.send({success: false, error: err})
     }
+    
 })
 
 // Get all posts from a given blog
 blogRouter.get("/:blogname/", async (req, res) => {
     try {
-        target = await Blog.findOne({where: {name: req.params.blogname}})
+        target = await Blog.findOne({where: {name: req.params.blogname}, include: Token})
         query = await target.getPosts()
-        res.send({success: true, body: query})
+        res.send({success: true, posts: query})
     } catch {
         res.send({success: false, error: "Something went wrong."})
     }
@@ -43,29 +52,26 @@ blogRouter.get("/:blogname/", async (req, res) => {
 
 // Create a new blog post
 blogRouter.put("/:blogname/posts", async (req, res) => {
-    try {
+ 
         // Check token
-        blog = await Blog.findOne({where: {name: req.params.blogname}})
-        blogTokens = await blog.getTokens()
-        tokens = blogTokens.find(element => {
-            element.token == req.body.token
+        const requestedBlog = await Blog.findOne({where: {name: req.params.blogname}})
+        const blogTokens = await requestedBlog.getTokens()
+        const tokens = blogTokens.find(element => {
+            return element.toJSON().token === req.body.token
         })
         
         // Empty array -> Unauthorized, non empty array -> Authorized
-        if (tokens.length == 0) { throw "Unauthorized" }
+        if (tokens.length === 0) { throw "Unauthorized" }
 
         // Find the blog to add the post to, and create the post
-        post = await Post.create({title: req.body.postTitle, content: req.body.postContent, creation_date: new Date()})
-        target = await Blog.findOne({where: {name: req.params.blogname}})
+        const post = await Post.create({title: req.body.postTitle, content: req.body.postContent, creation_date: new Date()})
+        const target = await Blog.findOne({where: {name: req.params.blogname}})
 
         // Add the post to the blog
         await target.addPost(post)
 
         // Send response
         res.send({success: true})
-    } catch (err) {
-        res.send({success: false, error:err})
-    }
 })
 
 // Given a token, return an address to their blog.
